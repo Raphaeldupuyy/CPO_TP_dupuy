a3651c7a1d2f1f01f8bf4feff279c8956a74f8bf
Rapahel <raphael.dupuy@epfedu.fr>
2023-10-15 23:16:51 +0200

    TP2 manip

---
+++ TP2_manip_DUPUY/src/tp2_manip_dupuy/Moussaka.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_dupuy;
+
+/**
+ *
+ * @author Raphaël
+ */
+public class Moussaka {
+        int nbCalories;
+
+ 
+
+    Moussaka(int calories) {
+
+        this.nbCalories = calories;
+
+    }
+    
+}
---
+++ TP2_manip_DUPUY/src/tp2_manip_dupuy/Tartiflette.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_dupuy;
+
+/**
+ *
+ * @author Raphaël
+ */
+public class Tartiflette {
+      int nbCalories;
+
+ 
+
+    Tartiflette(int calories) {
+
+        this.nbCalories = calories;
+
+    }
+}
